8bda3f24e696212f93716e2bbd67500a6d564e04
Xinyang Qiu <xqiu@outlook.com>
2013-01-08 10:31:24 -0800

    Add caching for RenderEmber template
    Add caching for RenderEmber template

--- MvcSPAWithEmberjs/Controllers/HtmlHelperExtensions.cs
+++ MvcSPAWithEmberjs/Controllers/HtmlHelperExtensions.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 
 namespace MvcHtmlHelpers
@@ -10,42 +13,57 @@ namespace MvcHtmlHelpers
 
         public static MvcHtmlString RenderEmber(this HtmlHelper helper, string path)
         {
-            string absolutePath;
-            string templateName = "";
-            if (string.IsNullOrEmpty(path))
+            if (HttpRuntime.Cache[path] == null)
             {
-                absolutePath = templateFolder;
-            }
-            else
-            {
-                templateName = path.Replace("\\", "-");
-                absolutePath = Path.Combine(templateFolder, path);
-            }
-
-            if (File.Exists(absolutePath))
-            {
-                int fileExtensionPosition = templateName.LastIndexOf('.');
-                if(fileExtensionPosition > 0)
+                string absolutePath;
+                string templateName = "";
+                if (string.IsNullOrEmpty(path))
                 {
-                    templateName = templateName.Substring(0, fileExtensionPosition);
+                    absolutePath = templateFolder;
                 }
-                return new MvcHtmlString(WrapWithHandlebarScript(templateName, absolutePath));
-            }
-
-            if (Directory.Exists(absolutePath))
-            {
-                if (templateName.Length > 0 && templateName[templateName.Length - 1] != '-')
+                else
                 {
-                    templateName += "-";
+                    templateName = path.Replace("\\", "-");
+                    absolutePath = Path.Combine(templateFolder, path);
                 }
-                return new MvcHtmlString(GetDirectoryTemplates(templateName, "", new DirectoryInfo(absolutePath)));
+
+                if (File.Exists(absolutePath))
+                {
+                    int fileExtensionPosition = templateName.LastIndexOf('.');
+                    if (fileExtensionPosition > 0)
+                    {
+                        templateName = templateName.Substring(0, fileExtensionPosition);
+                    }
+                    MvcHtmlString result = new MvcHtmlString(WrapWithHandlebarScript(templateName, absolutePath));
+                    HttpRuntime.Cache.Insert(path, result, new CacheDependency(absolutePath));
+                }
+                else
+                {
+                    if (Directory.Exists(absolutePath))
+                    {
+                        if (templateName.Length > 0 && templateName[templateName.Length - 1] != '-')
+                        {
+                            templateName += "-";
+                        }
+                        List<string> dependencyList = new List<string>();
+                        
+                        MvcHtmlString result = new MvcHtmlString(GetDirectoryTemplates(templateName, "", new DirectoryInfo(absolutePath), dependencyList));
+                        HttpRuntime.Cache.Insert(path, result, new CacheDependency(dependencyList.ToArray()));
+                    }
+                    else
+                    {
+                        return new MvcHtmlString(""); //nothing is found, return empty string and do not cache
+                    }
+                }
             }
 
-            return new MvcHtmlString("");
+            return HttpRuntime.Cache[path] as MvcHtmlString;
         }
 
-        private static string GetDirectoryTemplates(string templateName, string relativeDirName, DirectoryInfo rootDirectory)
+        private static string GetDirectoryTemplates(string templateName, string relativeDirName, DirectoryInfo rootDirectory, List<string> dependencyList)
         {
+            dependencyList.Add(rootDirectory.FullName);
+
             var newSubRelativeDirName = relativeDirName;
             if (!string.IsNullOrEmpty(newSubRelativeDirName))
             {
@@ -55,7 +73,7 @@ namespace MvcHtmlHelpers
 
             foreach (DirectoryInfo subDir in rootDirectory.GetDirectories())
             {
-                content += GetDirectoryTemplates(templateName, newSubRelativeDirName + subDir.Name, subDir);
+                content += GetDirectoryTemplates(templateName, newSubRelativeDirName + subDir.Name, subDir, dependencyList);
             }
 
             foreach (FileInfo templateFile in rootDirectory.GetFiles())
