ddb9eaef9ad66658f38031abf541e6c3bd06d244
Thiago Santos <thiago_trds@outlook.com>
2013-04-16 10:51:26 -0400

    pre-compile Handlebars Templates
    - Updated Ember.js to RC2, Ember-Data.js to revision 12 and jQuery to
    1.9.1
    - Added *.hbs tempaltes under ~/scripts/app/templates
    - Added [Handlebars
    compiler](https://github.com/Myslik/csharp-ember-handlebars)
    - Enabled Optimizations in Global.asax.cs
    - Refactored all bundles
    - Packages.config was missing ember, ember-data & handlebars
    - (Unfortunately) Had to upgrade Microsoft.AspNet.Web.Optimization to
    1.1.0b (reflected in packages.config)

--- MvcSPAWithEmberjs/App_Start/BundleConfig.cs
+++ MvcSPAWithEmberjs/App_Start/BundleConfig.cs
@@ -8,17 +8,28 @@ namespace MvcSPAWithEmberjs
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new Bundle("~/bundles/jquery", new JsMinify()).Include(
+                "~/Scripts/jquery-{version}.js"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new Bundle("~/bundles/jqueryval", new JsMinify()).Include(
+                "~/Scripts/jquery.unobtrusive*",
+                "~/Scripts/jquery.validate*"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new Bundle("~/bundles/templates", new EmberHandlebarsBundleTransform()).Include(
+                "~/scripts/app/templates/*.hbs"
+            ));
 
+            bundles.Add(new Bundle("~/bundles/ember", new JsMinify()).Include(
+                "~/scripts/handlebars.js",
+                "~/scripts/ember-1.0.0-rc.2.js",
+                "~/scripts/ember-data.js",
+                "~/scripts/app/webapi_serializer.js",
+                "~/scripts/app/webapi_adapter.js"
+            ));
 
+<<<<<<< HEAD
             bundles.Add(new ScriptBundle("~/bundles/ember").Include(
                 "~/Scripts/handlebars.js",
                 "~/Scripts/ember-1.0.0-rc.1.js",
@@ -30,36 +41,48 @@ namespace MvcSPAWithEmberjs
                 "~/Scripts/app/ajaxlogin.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
+=======
+            bundles.Add(new Bundle("~/bundles/ajaxlogin", new JsMinify()).Include(
+                "~/Scripts/app/ajaxlogin.js"
+            ));
+
+            bundles.Add(new Bundle("~/bundles/app", new JsMinify()).Include(
+>>>>>>> pre-compile Handlebars Templates
                 "~/Scripts/app/app.js",
                 "~/Scripts/app/router.js",
-                "~/Scripts/app/helpers.js")
-                .IncludeDirectory("~/Scripts/app/routes", "*.js")
-                .IncludeDirectory("~/Scripts/app/models", "*.js")
-                .IncludeDirectory("~/Scripts/app/views", "*.js")
-                .IncludeDirectory("~/Scripts/app/controllers", "*.js"));
+                "~/Scripts/app/helpers.js"
+                ).IncludeDirectory("~/Scripts/app/routes", "*.js")
+                 .IncludeDirectory("~/Scripts/app/models", "*.js")
+                 .IncludeDirectory("~/Scripts/app/views", "*.js")
+                 .IncludeDirectory("~/Scripts/app/controllers", "*.js")
+            );
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new Bundle("~/bundles/modernizr", new JsMinify()).Include(
+                "~/Scripts/modernizr-*"
+            ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new Bundle("~/Content/css", new CssMinify()).Include(
                 "~/Content/Site.css",
-                "~/Content/TodoList.css"));
+                "~/Content/TodoList.css"
+            ));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new Bundle("~/Content/themes/base/css", new CssMinify()).Include(
+                "~/Content/themes/base/jquery.ui.core.css",
+                "~/Content/themes/base/jquery.ui.resizable.css",
+                "~/Content/themes/base/jquery.ui.selectable.css",
+                "~/Content/themes/base/jquery.ui.accordion.css",
+                "~/Content/themes/base/jquery.ui.autocomplete.css",
+                "~/Content/themes/base/jquery.ui.button.css",
+                "~/Content/themes/base/jquery.ui.dialog.css",
+                "~/Content/themes/base/jquery.ui.slider.css",
+                "~/Content/themes/base/jquery.ui.tabs.css",
+                "~/Content/themes/base/jquery.ui.datepicker.css",
+                "~/Content/themes/base/jquery.ui.progressbar.css",
+                "~/Content/themes/base/jquery.ui.theme.css"
+            ));
+
         }
     }
 }
--- MvcSPAWithEmberjs/Global.asax.cs
+++ MvcSPAWithEmberjs/Global.asax.cs
@@ -23,6 +23,9 @@ namespace MvcSPAWithEmberjs
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            // required to precompile Handlebars templates into Ember.TEMPLATES
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
